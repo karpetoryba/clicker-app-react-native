007865816fe7d8f30a176d77b2aeb15cfbffc3dd
karpetoryba <karpenkokarpenko24@gmail.com>
2025-03-25 15:35:41 +0100

    new project

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,54 @@
+import { Image, StyleSheet, Platform, Button, Alert } from "react-native";
+
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { useState } from "react";
+
+export default function HomeScreen() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
+      headerImage={
+        <Image
+          source={require("@/assets/images/partial-react-logo.png")}
+          style={styles.reactLogo}
+        />
+      }
+    >
+      <Button
+        onPress={() => {
+          setCount(count + 1);
+        }}
+        title="Rouge"
+      />
+      <Button
+        onPress={() => {
+          setCount(count + 1);
+        }}
+        title="Bleu"
+      />
+      <ThemedText>You clicked me {count} times</ThemedText>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: "absolute",
+  },
+});
